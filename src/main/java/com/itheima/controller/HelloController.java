9109d6c15d2612bd3974d8b1e3c4ace5bd3f34ab
2223408299 <2223408299@qq.com>
2021-10-20 15:40:53 +0800

    first commit

---
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/HelloController")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println(123);
+        return "index";
+    }
+}
